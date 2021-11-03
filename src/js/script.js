97ca8b0fb8b7908b520d4c909833ada3c303cab4
Andrey <andreybatista131@gmail.com>
2021-11-03 12:29:44 -0300

    header_mobile

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const menuSize = '100%';
+
+let open = false;
+
+document.querySelector('#btnMenu').addEventListener('click', e => {
+    open = !open;
+    toggleMenu();
+})
+
+document.querySelector('#btnClose').addEventListener('click', e => {
+    open = false;
+
+    toggleMenu();
+})
+document.querySelector('.background-slider').addEventListener('click', e => {
+    open = false;
+
+    toggleMenu();
+})
+
+function toggleMenu() {
+    document.querySelector('.background-slider').style.display = "block";
+    if (open) {
+        document.querySelector('#slideMenu').style.left = "0%";
+        return;
+    }
+    document.querySelector('.background-slider').style.display = "none";
+    document.querySelector('#slideMenu').style.left = `-${menuSize}`;
+}
+
+$('.slider-principal').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="arrows-position slider-principal-prev"><img class="img-fluid" src="img/arrowleft-p.png"></button>',
+    nextArrow: '<button type="button" class="arrows-position slider-principal-next"><img class="img-fluid" src="img/arrowright-p.png"></button>',
+    dots: true,
+    dotsClass: 'dots-slider-principal',
+});
