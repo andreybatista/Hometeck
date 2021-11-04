ef049e06c11fe3d37ee02b8f1fcbacd65441ea18
andrey <andreybatista131@gmail.com>
2021-11-04 14:01:37 -0300

    bodyv3

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/mediaquery.css">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -181,13 +182,12 @@
         </svg>
     </a>
 
-    <button id="btnUp">
+    <a id="buttonTop">
         <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="24" cy="24" r="24" fill="black" fill-opacity="0.2" />
             <path d="M36 30L24 18L12 30" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
-    </button>
-    
+    </a>
 
     <footer></footer>
 
--- src/js/script.js
+++ src/js/script.js
@@ -5,82 +5,83 @@ const menuSize = '100%';
 let open = false;
 
 document.querySelector('#btnMenu').addEventListener('click', e => {
-    open = !open;
-    toggleMenu();
+  open = !open;
+  toggleMenu();
 })
 
 document.querySelector('#btnClose').addEventListener('click', e => {
-    open = false;
+  open = false;
 
-    toggleMenu();
+  toggleMenu();
 })
 document.querySelector('.background-slider').addEventListener('click', e => {
-    open = false;
+  open = false;
 
-    toggleMenu();
+  toggleMenu();
 })
 
 function toggleMenu() {
-    document.querySelector('.background-slider').style.display = "block";
-    if (open) {
-        document.querySelector('#slideMenu').style.left = "0%";
-        return;
-    }
-    document.querySelector('.background-slider').style.display = "none";
-    document.querySelector('#slideMenu').style.left = `-${menuSize}`;
+  document.querySelector('.background-slider').style.display = "block";
+  if (open) {
+    document.querySelector('#slideMenu').style.left = "0%";
+    return;
+  }
+  document.querySelector('.background-slider').style.display = "none";
+  document.querySelector('#slideMenu').style.left = `-${menuSize}`;
 }
 
 $('.slider-principal').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: '<button type="button" class="arrows-position slider-principal-prev"><img class="img-fluid" src="img/arrowleft-p.png"></button>',
-    nextArrow: '<button type="button" class="arrows-position slider-principal-next"><img class="img-fluid" src="img/arrowright-p.png"></button>',
-    dots: true,
-    dotsClass: 'dots-slider-principal',
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<button type="button" class="arrows-position slider-principal-prev"><img class="img-fluid" src="img/arrowleft-p.png"></button>',
+  nextArrow: '<button type="button" class="arrows-position slider-principal-next"><img class="img-fluid" src="img/arrowright-p.png"></button>',
+  dots: true,
+  dotsClass: 'dots-slider-principal',
+  enterMode: true,
+  variableWidth: false
 });
 
 $('.slider-produto').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: '<button type="button" class="arrows-position slider-principal-prev"><img class="img-fluid" src="img/arrowleft-produto.png"></button>',
-    nextArrow: '<button type="button" class="arrows-position slider-principal-next"><img class="img-fluid" src="img/arrowrigth-produto.png"></button>',
-    dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<button type="button" class="arrows-position slider-principal-prev"><img class="img-fluid" src="img/arrowleft-produto.png"></button>',
+  nextArrow: '<button type="button" class="arrows-position slider-principal-next"><img class="img-fluid" src="img/arrowrigth-produto.png"></button>',
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+  ]
 });
 
-var basicScrollTop = function () {  
-    // The button
-    var btnTop = document.querySelector('#btnUp');
-    // Reveal the button
-    var btnReveal = function () { 
-      var position = document.querySelector('#position');
-          position.innerHTML = window.scrollY + 'px';
-      if (window.scrollY >= 300) {
-        btnTop.classList.add('is-visible');
-      } else {
-        btnTop.classList.remove('is-visible');
-      }    
-    }  
-    // Smooth scroll top
-    // Thanks to => http://stackoverflow.com/a/22610562
-    var TopscrollTo = function () {
-      if(window.scrollY!=0) {
-        setTimeout(function() {
-          window.scrollTo(0,window.scrollY-30);
-          TopscrollTo();
-        }, 5);
-      }
-    }
-    // Listeners
-    window.addEventListener('scroll', btnReveal);
-    btnTop.addEventListener('click', TopscrollTo);  
-      
-  };
-  basicScrollTop();
-  
-  
-  
-  
+
+
+
+
+/* Scroll Top*/
+
+var btn = $('#buttonTop');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+
+
+/* Scroll Top*/
